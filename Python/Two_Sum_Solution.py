fc6e38ed89f0b07c8a157aef88e4184f454b4c70
BrianQu <qujianning@didiglobal.com>
2022-03-08 18:36:01 +0800

    Two Sum Solution

---
+++ Python/Two_Sum_Solution.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding=utf-8
+# @author qujianning
+# @date 2022-03-08 第1天
+# @input: nums = [2, 3, 4, 5]  target = 9
+# @brief: 给定一个数组和一个目标和，从数组中找两个数字相加等于目标和，输出这两个数字的下标
+# @output: [2, 3]
+
+def Two_Sum_Solution_1(nums, target):
+    '''
+        方法1: 使用暴力的方法，直接
+    '''
+
+if __name__ == "__main__":
+    nums = [2, 3, 4, 5]
+    target = 9
+    Two_Sum_Solution_1(nums, target)
